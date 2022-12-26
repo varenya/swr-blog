30f3523f4aea43cd5a2bf31381105550a715912c
Varenya Thyagaraj <varen90@gmail.com>
2022-12-26 13:02:32 +0530

    handle error schema error

--- src/components/Weather/WeatherError.tsx
+++ src/components/Weather/WeatherError.tsx
@@ -1,15 +1,30 @@
 import { ArrowPathIcon, XCircleIcon } from "@heroicons/react/20/solid";
 import { FallbackProps } from "react-error-boundary";
+import { z } from "zod";
+
+function getError(error: Error) {
+  if (error instanceof z.ZodError) {
+    return { message: "invalid data" };
+  }
+  switch (error.cause) {
+    case "network":
+    case "unavailable":
+      return { message: error.message };
+    default:
+      return { message: "something went wrong" };
+  }
+}
 
 function WeatherError({ error, resetErrorBoundary }: FallbackProps) {
   const showRetry = error.cause && error.cause == "network";
+  const { message } = getError(error);
   return (
     <div className="bg-white w-1/4 mx-auto rounded-lg p-8">
       <div className="rounded-md bg-red-50 p-6 mt-4">
         <div className="flex justify-center gap-4 flex-col">
           <div className="flex justify-center gap-2 items-center">
             <XCircleIcon className="h-8 w-8 text-red-400" aria-hidden="true" />
-            <h3 className="text-lg text-red-500">{error.message}</h3>
+            <h3 className="text-lg text-red-500">{message}</h3>
           </div>
           {showRetry ? (
             <div>
--- src/mock-api/index.ts
+++ src/mock-api/index.ts
@@ -5,20 +5,27 @@ function delay(time: number) {
 }
 async function mockApiResponse(location: string): Promise<Response> {
   switch (location) {
+    // positive case
     case "london":
       return new Response(
         JSON.stringify({ available: 1, humidity: 5, temperature: 20 })
       );
+    // invalid schema case
+    case "newyork":
+      return new Response(JSON.stringify({ available: 1 }));
+    // loading data from client case
     case "delhi":
       await delay(2000);
       return new Response(
         JSON.stringify({ available: 1, humidity: 10, temperature: 30 })
       );
+    // network error case
     case "istanbul":
       return new Response(JSON.stringify({ available: 0 }), {
         status: 502,
         statusText: "Server timed out",
       });
+    // data unavailable case
     case "mumbai":
       return new Response(JSON.stringify({ available: 0 }));
     default:
